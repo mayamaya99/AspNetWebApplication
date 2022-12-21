85c9eebbe197f543753d1892c77e976ccb961045
mayamaya99 <elmi33@hotmail.com>
2022-12-20 23:47:47 -0800

    Add ShowSearch method

--- Controllers/OfficeSpacesController.cs
+++ Controllers/OfficeSpacesController.cs
@@ -24,6 +24,11 @@ namespace AspNetWebApplication.Controllers
         {
             return View(await _context.OfficeSpace.ToListAsync());
         }
+        // GET: OfficeSpaces/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return View(await _context.OfficeSpace.ToListAsync());
+        }
 
         // GET: OfficeSpaces/Details/5
         public async Task<IActionResult> Details(int? id)
