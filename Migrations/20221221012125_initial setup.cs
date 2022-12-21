636417cba94d317ba59f5d26799301c93d1452ca
mayamaya99 <elmi33@hotmail.com>
2022-12-20 17:24:11 -0800

    Database migrations

---
+++ Migrations/20221221012125_initial setup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetWebApplication.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OfficeSpace",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PropertyType = table.Column<string>(nullable: true),
+                    PropertyPrice = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OfficeSpace", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OfficeSpace");
+        }
+    }
+}
